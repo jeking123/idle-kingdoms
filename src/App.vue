dd0148f8d8fe904e6b25cc99b3c4e404a6943976
jeremy <>
2018-08-19 12:11:08 -0700

    changes!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
 <<<<<<< HEAD
+<<<<<<< HEAD
   <div id="app" class="hero is-fullheight" style="position:relative;">
+=======
+  <div id="app" class="hero is-fullheight">
+>>>>>>> parent of 3b9c66a... testing the error over the whole app
     <app-header style="position:relative;"></app-header>
 =======
   <div id="app" class="hero is-fullheight">
@@ -8,16 +12,6 @@
 >>>>>>> parent of 8e5c86c... lots of updates
     <router-view class="box is-radiusless is-shadowless size-up is-marginless-bot is-paddingless-botm"></router-view>
     <app-footer class="bottom"></app-footer>
-    <div class="box is-radiusless is-overlay" style=" background: rgba(240,5,5,.4)">
-      <div class="hero">
-        <div class="has-text-dark hero-body is-paddingless-hori">
-          <div class="message message-header is-white has-text-dark is-fullwidth" style="width:100%;">
-            <p>this is an error</p>
-            <button class="delete" aria-label="delete"></button>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -60,8 +54,4 @@ export default {
   bottom: 0;
   width: 100%;
 }
-.is-paddingless-hori {
-    padding-left:0px;
-    padding-right:0px;
-  }
 </style>
