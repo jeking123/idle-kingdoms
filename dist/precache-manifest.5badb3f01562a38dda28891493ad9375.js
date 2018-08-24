5aea974de0472c5da42bc7e8ee4003d3801081df
jeremy <>
2018-08-24 16:11:22 -0700

    dist

---
+++ dist/precache-manifest.5badb3f01562a38dda28891493ad9375.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c48059792c3a9745bff007d28228dfbc",
+    "url": "/idle-kingdoms/assets/css/game-icons-font.css"
+  },
+  {
+    "revision": "e824c50de19162dfcc5c",
+    "url": "/idle-kingdoms/css/app.ccd5eda1.css"
+  },
+  {
+    "revision": "43ca4814222022eb2661",
+    "url": "/idle-kingdoms/js/chunk-vendors.43ca4814.js"
+  },
+  {
+    "revision": "7a00feee756fee8f32caf162078ca6ee",
+    "url": "/idle-kingdoms/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/idle-kingdoms/robots.txt"
+  },
+  {
+    "revision": "e824c50de19162dfcc5c",
+    "url": "/idle-kingdoms/js/app.e824c50d.js"
+  },
+  {
+    "revision": "a1906d0fdc93ae39f4db4773e2a15545",
+    "url": "/idle-kingdoms/assets/fonts/game-icons-font.woff2"
+  },
+  {
+    "revision": "45bc88337d3f7656a3c17ee5b4c97794",
+    "url": "/idle-kingdoms/assets/fonts/game-icons-font.woff"
+  },
+  {
+    "revision": "0e54b6272b59b285cba183ac1097874b",
+    "url": "/idle-kingdoms/assets/fonts/game-icons-font.ttf"
+  },
+  {
+    "revision": "f2898540daf251c3e75b5e4d65f44e21",
+    "url": "/idle-kingdoms/assets/fonts/game-icons-font.svg"
+  },
+  {
+    "revision": "665e87fa15741efd54fa751f826ca876",
+    "url": "/idle-kingdoms/assets/fonts/game-icons-font.eot"
+  },
+  {
+    "revision": "224d403e08babb2aa24887aa9e4caeb3",
+    "url": "/idle-kingdoms/assets/css/vue2-animate.min.css"
+  },
+  {
+    "revision": "b53f965411cef3887e6c1f15d94b055e",
+    "url": "/idle-kingdoms/assets/css/vue2-animate.css"
+  },
+  {
+    "revision": "b530050e80bb0a11adbd80ede1739d2d",
+    "url": "/idle-kingdoms/assets/css/game-icons-font.min.css"
+  }
+];
