3b6ebe31851e91067059bd42ded200dc197d4a03
jeremy <>
2018-08-24 16:10:48 -0700

    dist

---
+++ dist/precache-manifest.9753b4bc26796d93c8474a14162868b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a3d4ce071974c9bf6cb",
+    "url": "/idle-kingdoms/js/chunk-vendors-legacy.5a3d4ce0.js"
+  },
+  {
+    "revision": "c8d95e59b3c5d5216c08",
+    "url": "/idle-kingdoms/js/app-legacy.c8d95e59.js"
+  },
+  {
+    "revision": "9d0fdb6a32c43c7aed31f0b7ba8b039e",
+    "url": "/idle-kingdoms/index.html"
+  },
+  {
+    "revision": "c8d95e59b3c5d5216c08",
+    "url": "/idle-kingdoms/css/app.ccd5eda1.css"
+  }
+];
